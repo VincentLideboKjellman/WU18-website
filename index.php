f3a7eed65f42372c53b83ca4ffd553ee9ffaf5e6
Benjamin Fransson <benjamin@cafcon.se>
2018-10-03 14:10:28 +0200

    Changed page-title to homepage

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php 
-    $page_title = 'SQLite3 PDO';
+    $page_title = 'Homepage';
 
 	require('templates/header.php');
 	require('templates/nav.php');
 
 ?>
 
-<h1>We need some content here! :D</h1>
+<main>
+	<h1>We need some content here! :D</h1>
 
-<ul>
-	<li><a href="login.php">login</a></li>
-	<li><a href="register.php">register</a></li>
-</ul>
+	<ul>
+		<li><a href="login.php">login</a></li>
+		<li><a href="register.php">register</a></li>
+	</ul>
+</main>
 
 <?php 
     require('templates/footer.php');
